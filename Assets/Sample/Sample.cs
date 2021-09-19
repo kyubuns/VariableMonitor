3b762ef3e5e6e2dbacc9bce1073fe013e1054396
kyubuns <kyubuns@gmail.com>
2021-09-19 17:07:48 +0900

    feat: concept

--- Assets/Sample/Sample.cs
+++ Assets/Sample/Sample.cs
@@ -1,18 +1,61 @@
-using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using VariableMonitor;
 
 public class Sample : MonoBehaviour
 {
-    public IEnumerator Start()
+    [SerializeField] private Button variablePlusA;
+    [SerializeField] private Button variableMinusA;
+    [SerializeField] private Button variablePlusB;
+    [SerializeField] private Button variableMinusB;
+    [SerializeField] private Button variablePlusC;
+    [SerializeField] private Button variableMinusC;
+
+    public void Start()
     {
         Debug.Log("Start");
 
-        for (var i = 0; i < 10; ++i)
+        var variableA = 0;
+        var variableB = 0;
+        var variableC = 0;
+        VariableLogger.Log("VariableA", variableA);
+        VariableLogger.Log("VariableB", variableB);
+        VariableLogger.Log("VariableC", variableC);
+
+        variablePlusA.onClick.AddListener(() =>
         {
-            Debug.Log($"Update {i}");
-            VariableLogger.Log("VariableA", i);
-            yield return new WaitForSeconds(1f);
-        }
+            variableA++;
+            VariableLogger.Log("VariableA", variableA);
+        });
+
+        variableMinusA.onClick.AddListener(() =>
+        {
+            variableA--;
+            VariableLogger.Log("VariableA", variableA);
+        });
+
+        variablePlusB.onClick.AddListener(() =>
+        {
+            variableB++;
+            VariableLogger.Log("VariableB", variableB);
+        });
+
+        variableMinusB.onClick.AddListener(() =>
+        {
+            variableB--;
+            VariableLogger.Log("VariableB", variableB);
+        });
+
+        variablePlusC.onClick.AddListener(() =>
+        {
+            variableC++;
+            VariableLogger.Log("VariableC", variableC);
+        });
+
+        variableMinusC.onClick.AddListener(() =>
+        {
+            variableC--;
+            VariableLogger.Log("VariableC", variableC);
+        });
     }
 }
--- Assets/VariableMonitor/Editor/VariableMonitorWindow.cs
+++ Assets/VariableMonitor/Editor/VariableMonitorWindow.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace VariableMonitor.Editor
@@ -7,6 +8,8 @@ namespace VariableMonitor.Editor
     public class VariableMonitorWindow : EditorWindow
     {
         private IReadOnlyDictionary<string, string> _values;
+        private List<Item> _items;
+        private ReorderableList _reorderableList;
 
         [MenuItem("Window/Variable Monitor")]
         public static void ShowWindow()
@@ -16,13 +19,23 @@ namespace VariableMonitor.Editor
 
         public void OnEnable()
         {
-            Debug.Log("OnEnable");
             EditorApplication.playModeStateChanged += PlayModeStateChanged;
+
+            _items = new List<Item>();
+            _reorderableList = new ReorderableList(
+                elements: _items,
+                elementType: typeof(string),
+                draggable: false,
+                displayHeader: false,
+                displayAddButton: false,
+                displayRemoveButton: false
+            );
+            _reorderableList.draggable = true;
+            _reorderableList.drawElementCallback += DrawElementCallback;
         }
 
         public void OnDisable()
         {
-            Debug.Log("OnDisable");
             VariableLogger.OnUpdate -= OnUpdate;
             EditorApplication.playModeStateChanged -= PlayModeStateChanged;
         }
@@ -33,26 +46,48 @@ namespace VariableMonitor.Editor
             {
                 _values = VariableLogger.Values;
                 VariableLogger.OnUpdate += OnUpdate;
-                Repaint();
+                OnUpdate();
             }
         }
 
         private void OnUpdate(string key, string value)
         {
+            OnUpdate();
+        }
+
+        private void OnUpdate()
+        {
+            _items.Clear();
+            foreach (var v in _values)
+            {
+                _items.Add(new Item(v.Key, v.Value));
+            }
             Repaint();
         }
 
         public void OnGUI()
         {
-            if (_values == null) return;
-            foreach (var variable in _values)
+            _reorderableList.DoLayoutList();
+        }
+
+        private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            var v = _items[index];
+            EditorGUI.LabelField(rect, v.Key);
+
+            rect.xMin += 100;
+            EditorGUI.LabelField(rect, v.Value);
+        }
+
+        private class Item
+        {
+            public string Key { get; }
+            public string Value { get; }
+
+            public Item(string key, string value)
             {
-                using (new GUILayout.HorizontalScope())
-                {
-                    GUILayout.Label(variable.Key);
-                    GUILayout.FlexibleSpace();
-                    GUILayout.Label(variable.Value);
-                }
+                Key = key;
+                Value = value;
             }
         }
     }
